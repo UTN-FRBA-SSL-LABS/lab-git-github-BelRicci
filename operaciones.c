4a6e2f40f037556e9811f55f3c495cd84ad544bf
Joaquin Abalos <jabalos@frba.utn.edu.ar>
2026-05-02 00:50:31 -0300

    Atiende review: <cambie las descripciones de las funciones>

--- operaciones.c
+++ operaciones.c
@@ -1,14 +1,14 @@
 #include "operaciones.h"
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
-
+/* sirve para combinar o añadir dos o más valores numéricos (enteros o de coma flotante) para obtener un total a + b */
 int sumar(int a, int b)
 {
     return a + b;
 }
 
 /* ── restar — ya implementada ────────────────────────────────────────────── */
-
+/* sirve para calcular la diferencia entre dos valores numéricos (enteros o de punto flotante) a - b */
 int restar(int a, int b)
 {
     return a - b;
@@ -22,7 +22,7 @@ int multiplicar(int a, int b)
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
-
+/* para verificar si un número entero es divisible exactamente entre 2*/
 int esPar(int n)
 {
     return (n % 2) == 0;
